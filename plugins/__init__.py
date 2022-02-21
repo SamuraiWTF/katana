ac4ddd0dcb76c3b7ffa47968832741f71431bd05
Jason <jason.gillam@owasp.org>
2022-02-21 15:36:40 -0500

    Add replace Plugin

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -5,6 +5,7 @@ from .Copy import Copy
 from .Docker import Docker
 from .Git import Git
 from .Remove import Remove
+from .Replace import Replace
 from .Command import Command
 from .Unarchive import Unarchive
 from .ReverseProxy import ReverseProxy
